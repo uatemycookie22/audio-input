2265c40decdcae5930f0cf74d9f93acb70ffd950
uatemycookie22 <pairofkings30@gmail.com>
2021-07-13 09:34:51 -0500

    Rewritten complete. Can format to JSON with more utility.

--- scripts/util.js
+++ scripts/util.js
@@ -1,44 +1,46 @@
 #! /usr/bin/env node
 
-const {existsSync, readFileSync} = require('fs');
-const jsonfile = require('jsonfile');
-const { Observable, forkJoin } = require('rxjs');
-const cp = require('child_process'),
-exec = cp.exec;
-
-var dotenv = require('dotenv');
-
 process.argv.forEach(function (val,index, array) {
 	console.log(index + ': ' + val);
 });
 
-if(existsSync('.env-local')) {
-  const localEnv = dotenv.parse(readFileSync('.env-local'));
-  for(var i in localEnv) {
-    process.env[i] = localEnv[i];
-  }
-}
-
-//console.log(process.env)
-console.log(process.argv)
-const func = process.env.npm_config_func || 'none';
+//console.log('Printing env args...')
+//console.log(process.env.npm_config_task)
+//console.log(process.env.npm_config_name)
+//console.log(process.env.npm_config_type)
+//console.log(process.env.npm_config_audio_file)
+//console.log('Printing argv args...')
+//console.log(process.argv)
+const func = process.env.npm_config_task || 'none';
 
 
 
 let funcs = {
 	format: () =>{
 		const audio = process.env.npm_config_audio_file;
+		const name = process.env.npm_config_name;
+		const type = process.env.npm_config_type;
+		
+		console.log('Formatting ' + name)
 
 		if (!audio){
 			console.log('Missing arguments. No such audio file exists.');
 		}
-		const audio64 = Buffer.from(audio).toString('base64');
+		
+		const binary_data = require('fs').readFileSync(audio)
+		const audio64 = Buffer.from(binary_data).toString('base64');
+		//console.log(audio64);
                 var audio_obj = {
-                	audio: audio64
+			name: name,
+                	audio: audio64,
+			type: type
                 };
                 const audio_json = JSON.stringify(audio_obj);
-
-		return audio_json 
+		//console.log(audio_json)
+		
+		const full_name = name + '.json'
+		require('fs').writeFileSync(full_name, audio_json);
+		console.log(full_name + ' successfully created');
 	},
 	none: function(){
 		console.log('No such task. Available tasks:\nformat [audio_file].');
